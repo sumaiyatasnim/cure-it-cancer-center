790d546c03247062bf58cd50366c6784e411ea4d
sumaiyatasnim <tasnimsumaiya07@gmail.com>
2021-10-20 10:30:32 -0700

    contact-style changed

--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -3,10 +3,12 @@ import './contact.css'
 
 const ContactUs = () => {
     return (
-        <div className="container contact-container mt-5">
-            <h3 >Phone no: 833-920-3234 , 888-675-7722</h3>
-            <p > Email: cureItHospital@gmail.com</p>
-            <p >Adress: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto qui optio reiciendis in </p>
+        <div className="container contact-container ">
+            <div className="mt-5">
+                <h3 className="pt-5">Phone no: 833-920-3234 , 888-675-7722</h3>
+                <p > Email: cureItHospital@gmail.com</p>
+                <p >Address: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto qui optio reiciendis in </p>
+            </div>
         </div>
     );
 };
